56cac5c21be84f4e03eb8654a5810e9d9f64f1bd
Super <Hamid82420@gmail.com>
2025-12-01 12:49:40 -0500

    Update hand.h

--- hand.h
+++ hand.h
@@ -1,1 +1,21 @@
+//Hand.h
 
+#ifndef HAND_H
+#define HAND_H
+
+#include <vector>
+#include "Card.h"
+
+class Hand {
+private:    
+    std::vector<Card> cards;
+
+public:
+    void addCard(const Card& c);
+    int getScore() const;
+    void clear();
+    void sortDescending();
+    void showHand(bool showAll) const;
+};
+
+#endif
