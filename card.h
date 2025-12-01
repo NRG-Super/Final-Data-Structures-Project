dc8c6733b384842e920f7bf6737f87173f10c9af
Super <Hamid82420@gmail.com>
2025-12-01 12:30:19 -0500

    Add Card class definition in card.h

--- card.h
+++ card.h
@@ -1,1 +1,24 @@
+//Card.h
+
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+
+class Card {
+private:
+    std::string rank;
+    char suit;
+    int value;
+
+public:
+    Card(const std::string& r = "?", char s = "?", int v = 0);
+
+    int getValue() const;
+    bool isAce() const;
+    std::string toString() const;
+};
+
+
+#endif
 
