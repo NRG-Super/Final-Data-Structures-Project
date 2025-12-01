096895876a5147ccf15e215012bdfc9961fb2342
Super <Hamid82420@gmail.com>
2025-12-01 12:37:30 -0500

    Add Deck class definition with basic methods

--- deck.h
+++ deck.h
@@ -1,1 +1,22 @@
+//Deck.h
 
+#ifndef DECK_H
+#define DECK_H
+
+#include <vector>
+#include "Card.h"
+
+class Deck {
+private:
+    std::vector<Card> cards;
+
+public:
+    Deck();
+
+    void reset();
+    void shuffleDeck();
+    Card dealCard();
+    bool isEmpty() const;
+};
+
+#endif
