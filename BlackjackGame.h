30c11964bacd9661f7eb94bb1ac7b05e6c13894f
Super <Hamid82420@gmail.com>
2025-12-01 13:01:09 -0500

    Add BlackjackGame class with playRound method

--- BlackjackGame.h
+++ BlackjackGame.h
@@ -1,1 +1,18 @@
+//BlackjackGame.h
 
+#ifndef BLACKJACKGAME_H
+#define BLACKJACKGAME_H
+
+#include "Deck.h"
+#include "Hand.h"
+
+class BlackjackGame {
+private:
+    Deck deck;
+    Hand player;
+    Hand dealer;
+public:
+    void playRound();
+};
+
+#endif
