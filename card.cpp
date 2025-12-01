40064b660302f6ed713ec62fb928ff30617baff8
Super <Hamid82420@gmail.com>
2025-12-01 12:33:57 -0500

    Implement Card class with basic functionality

--- card.cpp
+++ card.cpp
@@ -1,1 +1,17 @@
+//Card.cpp
 
+#include "Card.h"
+
+Card::Card(const std::string& r, char s, int v) : rank(r), suit(s), value(v) {}
+
+int Card::getValue() const {
+    return value;
+}
+
+bool Card::isAce() const {
+    return rank == "A";
+}
+
+std::string Card::toString() const {
+    return rank + "-" + suit;
+}
