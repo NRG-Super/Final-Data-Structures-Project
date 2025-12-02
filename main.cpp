ac25adb721265316eefb6ad6b8d3123af10f830a
Super <Hamid82420@gmail.com>
2025-12-02 14:57:48 -0500

    Implement main function for Blackjack game

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+//main.cpp
 
+#include <iostream>
+#include "BlackjackGame.h"
+
+using namespace std;
+
+int main () {
+    BlackjackGame game;
+    char again;
+
+    cout << "==== Blackjack Game ====\n";
+
+    do {
+        game.playRound();
+        cout << "\nPlay again? (y/n): ";
+        cin >> again;
+    } while (again == 'y' || again == 'Y');
+
+    cout << "\nThanks for playing! \n";
+    return 0;
+}
