242628664a9fb426e88b8eaee4f8f479030540cb
Super <Hamid82420@gmail.com>
2025-12-08 12:35:09 -0500

    Refactor Hand class to use linked list for cards

--- hand.cpp
+++ hand.cpp
@@ -4,49 +4,98 @@
 
 using namespace std;
 
-void Hand::addCard(const Card& c) {
-    cards.push_back(c);
+//Constructor initializes empty list
+Hand::Hand() : head(nullptr) {}
+
+//Destructor frees all allocated nodes
+Hand::~Hand() {
+    clear();
 }
 
+//Adds a card to the end of the linked list
+void Hand::addCard(const Card& c) {
+    Node* newNode = new Node(c);
+
+    //If list emptry, new node becomes head
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    //Traverse to end of list
+    Node* curr = head;
+    while (curr->next != nullptr) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+}
+
+//Compute score with Ace logic
 int Hand::getScore() const {
     int total = 0;
     int aceCount = 0;
 
-    for (const Card& c : cards) {
-        total += c.getValue();
-        
-        if (c.isAce()) aceCount ++;
+    Node* curr = head;
+    while (curr != nullptr) {
+        total += curr->data.getValue();
+        if (curr->data.isAce()) aceCount++;
+        curr = curr->next;
     }
 
+    //Adjust Ace values if necessary
     while (total > 21 && aceCount > 0) {
-        total -=10;
+        total -= 10;
         aceCount--;
     }
     return total;
 }
 
+//Removes all nodes from the linked list
 void Hand::clear() {
-    cards.clear();
+    Node* curr = head;
+    while (curr != nullptr) {
+        Node* temp = curr;
+        curr = curr->next;
+        delete temp;
+    }
+    head = nullptr;
 }
 
 void Hand::sortDescending() {
-    sort(cards.begin(), cards.end(), [](const Card& a, const Card& b) {
-        return a.getValue() > b.getValue();
-    });
+    if (!head || !head->next) return;   //0 or 1 element
+
+    bool swapped;
+    do {
+        swapped = false;
+        Node* curr = head;
+        while (curr->next != nullptr) {
+            if (curr->data.getValue() < curr->next->data.getValue()) {
+                swap(curr->data, curr->next->data);
+                swapped = true;
+            }
+            curr = curr->next;
+        }
+    } while (swapped);
 }
 
+//Display cards, hide 2nd dealer card if required
 void Hand::showHand(bool showAll) const {
-    for (size_t i = 0; i < cards.size(); i++) {
-        if (!showAll && i == 1) 
-            cout << "[ ? ] ";
+    Node* curr = head;
+    int index = 0;
+
+    while (curr != nullptr) {
+        if (!showAll && index == 1)
+            cout << "[ ? ]";
         else 
-        cout << "[" << cards[i].toString() << "] ";
+            cout << "[" << curr->data.toString() << "] ";
+
+        curr = curr->next;
+        index++;
     }
 
     if (showAll)
-        cout << "(Score : " << getScore() << ")";
-    
+        cout << "(Score: " << getScore() << ")";
+
     cout << endl;
 }
-
 
