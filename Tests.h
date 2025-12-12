e71191ca492be0c19ab3c505e61afd62ba570f85
Super <Hamid82420@gmail.com>
2025-12-12 14:41:17 -0500

    Add header file for Blackjack test cases

---
+++ Tests.h
@@ -0,0 +1,14 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+//Runs all Blackjack test cases
+void runAllTests();
+
+//Individual test cases
+void testAddCard();
+void testAceScoring();
+void testSortingAscending();
+void testDealerLogic();
+void testMultipleRounds();
+
+#endif
