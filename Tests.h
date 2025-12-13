a8448bc93768569fb34b2e62746cc61ddf041338
picklepiepizza <37784720+picklepiepizza@users.noreply.github.com>
2025-12-12 21:31:06 -0500

    Remove comment from Tests.h
    Removed comment about running all Blackjack test cases.

--- Tests.h
+++ Tests.h
@@ -1,10 +1,8 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-//Runs all Blackjack test cases
 void runAllTests();
 
-//Individual test cases
 void testAddCard();
 void testAceScoring();
 void testSortingAscending();
